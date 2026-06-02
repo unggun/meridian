de8687ad960d9e64ae17b839d716c29eb3b57da9
unggun <andreas.unggun@gmail.com>
2026-06-02 12:05:07 +0200

    feat: add pullback tuning config keys (lookback/dip/reclaim)

--- tools/executor.js
+++ tools/executor.js
@@ -451,6 +451,9 @@ const toolMap = {
       rsiOverbought: ["indicators", "rsiOverbought", ["chartIndicators", "rsiOverbought"]],
       maxEntryRsi: ["indicators", "maxEntryRsi", ["chartIndicators", "maxEntryRsi"]],
       requireAllIntervals: ["indicators", "requireAllIntervals", ["chartIndicators", "requireAllIntervals"]],
+      pullbackLookbackBars: ["indicators", "pullbackLookbackBars", ["chartIndicators", "pullbackLookbackBars"]],
+      pullbackDipBand: ["indicators", "pullbackDipBand", ["chartIndicators", "pullbackDipBand"]],
+      pullbackReclaimBand: ["indicators", "pullbackReclaimBand", ["chartIndicators", "pullbackReclaimBand"]],
       supertrendExitEnabled: ["indicators", "supertrendExitEnabled", ["chartIndicators", "supertrendExitEnabled"]],
       supertrendExitInterval: ["indicators", "supertrendExitInterval", ["chartIndicators", "supertrendExitInterval"]],
     };
